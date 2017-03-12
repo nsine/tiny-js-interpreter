b0ca008ba55462c4a82450aa20d8cc0339c31372
Vlad Klimenkov <Vlad Klimenkov>
2017-03-12 22:52:07 +0300

    Add 'for' cycle, arrays, exceptions.

--- test/test.py
+++ test/test.py
@@ -1,6 +1,15 @@
 user_input = input('Enter array items through space: ')
 arr_items = user_input.split(' ')
+arr = []
+
+for item in arr_items:
+    arr.append(float(item))
+
 length = len(arr)
 sum = 0
+
+for i in range(length):
+    sum += arr[i]
+
 avg = sum / length
 print(avg)
